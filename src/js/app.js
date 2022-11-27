2057998a73a5a71235c80d3c833572955fa9a5af
NanoProgram <danielj.perezd@gmail.com>
2022-11-27 21:27:25 +0000

    save2

--- src/js/app.js
+++ src/js/app.js
@@ -38,10 +38,10 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city} ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fa fa-twitter"></i>${variables.twitter}</a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i>${variables.github}</a></li>
+            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i>${variables.linkedin}</a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i>${variables.instagram}</a></li>
           </ul>
         </div>
     `;
